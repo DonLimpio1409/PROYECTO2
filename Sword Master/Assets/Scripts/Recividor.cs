1f6fb7bb73cefa63a05455933ef1a229b440757f
DonLimpio1409 <ppn1002@alu.ubu.es>
2025-09-25 20:45:46 +0200

    Sumar

--- Sword Master/Assets/Scripts/Recividor.cs
+++ Sword Master/Assets/Scripts/Recividor.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
 using UnityEngine;
+using System;
+using System.Net;
 
 public class Recividor : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public UdpClient udp;
+    public IPEndPoint remoteEndPoint;
+
+    public Quaternion ultimaRotacion { get; private set; }
+    public Transform posicion { get; private set; }
+
     void Start()
     {
-        
+        udp = new UdpClient(5055);
+        remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (udp.Available > 0)
+        {
+            byte[] data = udp.Receive(ref remoteEndPoint);
+            string mensaje = Encoding.UTF8.GetString(data);
+
+            Debug.Log("" + mensaje);
+
+            MotionData m = JsonUtility.FromJson<MotionData>(mensaje);
+            ultimaRotacion = new Quaternion(m.x, m.y, m.z, m.w);
+        }
+    }
+
+    [System.Serializable]
+
+    public class MotionData
+    {
+        public float x, y, z, w;
+        public float ax, ay, az;
     }
 }
