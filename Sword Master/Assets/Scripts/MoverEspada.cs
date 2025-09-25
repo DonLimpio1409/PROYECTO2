143d2a3a97c7c3c4b428b19539c1e4ada7304ad0
DonLimpio1409 <ppn1002@alu.ubu.es>
2025-09-25 20:45:46 +0200

    Sumar

---
+++ Sword Master/Assets/Scripts/MoverEspada.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverEspada : MonoBehaviour
+{
+    private Recividor recividor;
+
+    // Start is called before the first frame update 
+    void Start()
+    {
+        recividor = FindObjectOfType<Recividor>();
+    }
+
+    // Update is called once per frame 
+    void Update()
+    {
+        if (recividor != null)
+        {
+            transform.rotation = new Quaternion(-recividor.ultimaRotacion.x, -recividor.ultimaRotacion.y, recividor.ultimaRotacion.z, recividor.ultimaRotacion.w);
+        }
+    }
+}
