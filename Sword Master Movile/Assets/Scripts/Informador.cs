d61e9c66ff84c61c91c29262984eed9c3bbb7f8d
DonLimpio1409 <ppn1002@alu.ubu.es>
2026-02-23 16:40:07 +0100

    Nuevo proyecto

---
+++ Sword Master Movile/Assets/Scripts/Informador.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Net.Sockets;
+using System.Text;
+
+public class Informador : MonoBehaviour
+{
+    public string ipPC = "192.168.1.1";
+    public int puerto = 5005;
+
+    public Button botonDefensa;
+
+    UdpClient udp;
+    bool defendiendo = false;
+
+    void Start()
+    {
+        udp = new UdpClient();
+
+        if (SystemInfo.supportsGyroscope)
+            Input.gyro.enabled = true;
+
+        botonDefensa.onClick.AddListener(() =>
+        {
+            defendiendo = true;
+            Invoke(nameof(DesactivarDefensa), 0.3f);
+        });
+    }
+
+    void DesactivarDefensa()
+    {
+        defendiendo = false;
+    }
+
+    void Update()
+    {
+        Quaternion rot = Input.gyro.attitude;
+        Vector3 acc = Input.acceleration;
+
+        string mensaje =
+            rot.x + "," + rot.y + "," + rot.z + "," + rot.w + "," +
+            acc.x + "," + acc.y + "," + acc.z + "," +
+            defendiendo;
+
+        byte[] datos = Encoding.UTF8.GetBytes(mensaje);
+        udp.Send(datos, datos.Length, ipPC, puerto);
+    }
+}
