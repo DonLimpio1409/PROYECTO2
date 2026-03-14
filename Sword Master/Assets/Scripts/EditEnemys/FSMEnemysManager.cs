a2d8f16d963d1f74befaa728f8e508472f8de897
DonLimpio1409 <ppn1002@alu.ubu.es>
2026-03-14 14:12:52 +0100

    IA Enemigos
    Empezar la IA de los enemigos y cambiar todo a ingles.

---
+++ Sword Master/Assets/Scripts/EditEnemys/FSMEnemysManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSMEnemysManager : StateMachineFlow
+{
+    public Idle idleState;
+    public Patrol patrolState;
+    private void Awake()
+    {
+        idleState = new Idle(this);
+        patrolState = new Patrol(this);
+    }
+    protected override void GetInitialState(out TemplateStateMachine _stateMachine)
+    {
+        // Asignar un valor a la variable de salida
+        _stateMachine = idleState;
+        _stateMachine = patrolState;
+    }
+
+    //Elements
+    public List<Material> materialEstados = new List<Material>();
+    public Renderer rend;
+    public Rigidbody rb = new Rigidbody();
+
+
+    public bool goWalk;
+    public WayPopintData waypointData;
+    public float speed = 3;
+
+    public IEnumerator WaitForPatrol()
+    {
+        yield return new WaitForSeconds(3f);
+        goWalk = true;
+    }
+}
+
