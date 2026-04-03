3647921bfa2fcbf74b983700c18ce07692948413
DonLimpio1409 <ppn1002@alu.ubu.es>
2026-04-03 14:22:07 +0200

    Creacion de Chase
    He afinado el Patrol y he Empezado el Chase y el movimiento del jugador

--- Sword Master/Assets/Scripts/EditEnemys/FSMEnemysManager.cs
+++ Sword Master/Assets/Scripts/EditEnemys/FSMEnemysManager.cs
@@ -9,34 +9,41 @@ public class FSMEnemysManager : StateMachineFlow
     //Estados
     public Idle idleState;
     public Patrol patrolState;
+    public Chase chaseState;
     private void Awake()
     {
         //Declaracion de estados
         idleState = new Idle(this);
         patrolState = new Patrol(this);
+        chaseState = new Chase(this);
     }
     protected override void GetInitialState(out TemplateStateMachine _stateMachine)
     {
         // Asignar un valor a la variable de salida
         _stateMachine = idleState;
         _stateMachine = patrolState;
+        _stateMachine = chaseState;
     }
 
-    //Elementos para los estados
-    public List<Material> materialEstados = new List<Material>();
-    public Renderer rend;
+    [Header("Elementos de uso")]
     public Rigidbody rb = new Rigidbody();
     public TextMeshProUGUI stateNameT;
 
-    //Patrol
+    [Header("Patrol")]
     public bool goIdle;
     public bool changeWayPoint;
     public WayPopintData waypointData;
     public float speed = 3;
     public int currentWayPointIndex = 0;
+    public int waitTimeR = 1;
     public GameObject waypoint1;
     public GameObject waypoint2;
     public GameObject waypoint3;
+
+    [Header("Detectar al jugador")]
+    public Ray rayDetector = new Ray();
+    public RaycastHit hit;
+    public float rayLength = 2f;
 
     public void OnTriggerEnter(Collider other)
     {
--- Sword Master/Assets/Scripts/EditEnemys/Patrol.cs
+++ Sword Master/Assets/Scripts/EditEnemys/Patrol.cs
@@ -48,9 +48,9 @@ public class Patrol : TemplateStateMachine
     public void IniciateWayPoints()
     {
         _fsm.waypointData.wayPointList.Clear();
-        _fsm.waypointData.AddWayPoint(new Vector3(_fsm.waypoint1.transform.position.x, _fsm.waypoint1.transform.position.y, _fsm.waypoint1.transform.position.z), 3f);
-        _fsm.waypointData.AddWayPoint(new Vector3(_fsm.waypoint2.transform.position.x, _fsm.waypoint2.transform.position.y, _fsm.waypoint2.transform.position.z), 3f);
-        _fsm.waypointData.AddWayPoint(new Vector3(_fsm.waypoint3.transform.position.x, _fsm.waypoint3.transform.position.y, _fsm.waypoint3.transform.position.z), 3f);
+        _fsm.waypointData.AddWayPoint(new Vector3(_fsm.waypoint1.transform.position.x, _fsm.waypoint1.transform.position.y, _fsm.waypoint1.transform.position.z), _fsm.waitTimeR);
+        _fsm.waypointData.AddWayPoint(new Vector3(_fsm.waypoint2.transform.position.x, _fsm.waypoint2.transform.position.y, _fsm.waypoint2.transform.position.z), _fsm.waitTimeR);
+        _fsm.waypointData.AddWayPoint(new Vector3(_fsm.waypoint3.transform.position.x, _fsm.waypoint3.transform.position.y, _fsm.waypoint3.transform.position.z), _fsm.waitTimeR);
 
         target = _fsm.waypointData.wayPointList[_fsm.currentWayPointIndex];
     }
@@ -63,12 +63,31 @@ public class Patrol : TemplateStateMachine
             _fsm.currentWayPointIndex = (_fsm.currentWayPointIndex + 1) % _fsm.waypointData.wayPointList.Count;
 
             target = _fsm.waypointData.wayPointList[_fsm.currentWayPointIndex];
+
             _fsm.changeWayPoint = false;
         }
     }
 
     public void MoveToPosition()
     {
+        //Mover
         _fsm.transform.position = Vector3.MoveTowards(_fsm.transform.position, target.wayPointPosition, _fsm.speed * Time.deltaTime);
+
+        //Rotar sin que se caiga
+        Vector3 direction = _fsm.rb.position - target.wayPointPosition;
+        direction.y = 0;
+
+        if (direction != Vector3.zero)
+        {
+            Quaternion rotation = Quaternion.LookRotation(direction);
+
+            Vector3 euler = rotation.eulerAngles;
+            euler.x = 0;
+            euler.z = 0;
+
+            rotation = Quaternion.Euler(euler);
+
+            _fsm.transform.rotation = Quaternion.Slerp(_fsm.transform.rotation, rotation, 3 * Time.deltaTime);
+        }
     }
 }
