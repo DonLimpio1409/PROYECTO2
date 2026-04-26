912fdbce165a898dcabfe559519c7721b961741e
DonLimpio1409 <ppn1002@alu.ubu.es>
2026-04-26 18:56:21 +0200

    Combate terninado

--- Sword Master/Assets/Scripts/Enemies/EditEnemys/Combat.cs
+++ Sword Master/Assets/Scripts/Enemies/EditEnemys/Combat.cs
@@ -19,22 +19,33 @@ public class Combat : TemplateStateMachineEnemies
         _fsm.anim.SetBool("Combat", true);
         _fsm.anim.SetBool("Walking", false);
         _fsm.anim.SetBool("Surprise", false);
+
+        _fsm.bloking = true; 
     }
 
     public override void UpdateLogic()
     {
         base.UpdateLogic();
+        _fsm.rdn = Random.Range(0, _fsm.hitProbably);
+        if(_fsm.rdn == 0)
+        {
+            Hit();
+        }
+        Die();
     }
 
     public override void UpdatePhysics()
     {
         base.UpdatePhysics();
+        //Rotar
         Vector3 direction = _fsm.player.transform.position - _fsm.transform.position;
         direction.y = 0f;
         Quaternion objective = Quaternion.LookRotation(-direction);
-
         _fsm.transform.rotation = Quaternion.Lerp(_fsm.transform.rotation, objective, Time.deltaTime * 20f);
+    }
 
+    public void Die()
+    {
         if (_fsm.upEnemy <= 0)
         {
             _fsm.gameObject.GetComponent<FSMEnemysManager>().enabled = false;
@@ -44,5 +55,24 @@ public class Combat : TemplateStateMachineEnemies
             _fsm.rb.AddForce(20f, 0, 0);
             _fsm.anim.SetBool("Die", true);
         }
+    }
+
+    public void Hit()
+    {
+        _fsm.anim.SetBool("Hit", true);
+
+        if(_fsm.player.GetComponent<FSMPlayerManager>().blocking)
+        {
+            _fsm.bloking = false;
+        }
+        else
+        {
+            _fsm.player.GetComponent<FSMPlayerManager>().hp -= 1;   
+            _fsm.img.GetComponent<Animator>().SetBool("Damage", true);
+        }
+
+        _fsm.punch = false;
+        _fsm.anim.SetBool("Hit", false);
+        _fsm.img.GetComponent<Animator>().SetBool("Damage", false);
     }
 }
